986c3a486ad56f29fcd4fcd44928393ee43899c3
dahliacreative <simon@dahliacreative.com>
2016-09-23 08:54:55 +0100

    imagemin

--- grunt/imagemin.js
+++ grunt/imagemin.js
@@ -10,7 +10,7 @@ module.exports = {
     files: [{
       expand: true,
       cwd: '<%= config.buildFolder %>/app/images',
-      src: ['**/*.{png,jpg,gif,svg}'],
+      src: ['**/*.{png,jpg,gif}'],
       dest: '<%= config.buildFolder %>/app/images'
     }]
   }
