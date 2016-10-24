db44899da6685fd9943fbd9116ecc2d2e2b893d0
Simon Sturgess <dahliacreative@users.noreply.github.com>
2016-10-24 14:25:44 +0100

    Update doc_header.php

--- application/themes/rawnet/includes/doc_header.php
+++ application/themes/rawnet/includes/doc_header.php
@@ -2,12 +2,17 @@
     defined('C5_EXECUTE') or die("Access Denied.");
     use Application\Src\Helpers\SEOHelper;
     $c = Page::getCurrentPage();
+    $u = new User();
 ?>
 
 <!DOCTYPE html>
 <html lang="en" class="no-js no-touch">
     <head>
-        <?php Loader::element('header_required');?>
+        <?php 
+            if($u->IsLoggedIn()) {
+                Loader::element('header_required');
+            }
+        ?>
         <meta property="og:title" content="<?php echo $c->getCollectionAttributeValue('meta_title');?>" />
         <meta property="og:url" content="http://www.peggysuesconfectionery.co.uk" />
         <meta property="og:description" content="<?php echo $c->getCollectionAttributeValue('meta_description'); ?>" />
